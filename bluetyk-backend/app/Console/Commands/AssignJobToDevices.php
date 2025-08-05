5793f83d0f6b444bdf6ccffe6601ce5b01b7cfd9
basith <basithmuhammadbasith2@gmail.com>
2025-08-05 11:53:53 +0530

    cron job

---
+++ bluetyk-backend/app/Console/Commands/AssignJobToDevices.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\AttendanceLogTimeTracker;
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Exceptions\Renderer\Exception;
+use Illuminate\Support\Facades\Log;
+
+class AssignJobToDevices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:assign-job-to-devices';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        try {
+            AttendanceLogTimeTracker::AttendaceCommand();
+            Log::info("Assigned job successfully");
+        } catch (Exception $e) {
+            Log::error(" failed to Assign the job ");
+        }
+    }
+}
--- bluetyk-backend/app/Http/Controllers/BiometricDeviceController.php
+++ bluetyk-backend/app/Http/Controllers/BiometricDeviceController.php
@@ -62,11 +62,11 @@ class BiometricDeviceController extends Controller
     /**
      * Handle GET /iclock/cdata.aspx — important for initial handshake
      */
-     public function handleCdataGet(Request $request){
+    public function handleCdataGet(Request $request)
+    {
 
-      return response("OK", 200)->header('Content-Type', 'text/plain');
-
-     }
+        return response("OK", 200)->header('Content-Type', 'text/plain');
+    }
 
     /**
      * handle the post method /iclock/cdata.aspx / /iclock/devicecmd.aspx
@@ -156,6 +156,7 @@ class BiometricDeviceController extends Controller
         Members::addingMemberToDevice();
         DeviceUserLogs::updateSuccessStatus();
         DeviceUserLogs::updateDeletedStatus();
+        DeviceUserLogs::addUserDeviceToApp();
         AttendanceLogTimeTracker::AttendaceCommand();
 
         return response('OK', 200)->header('Content-Type', 'text/plain');
